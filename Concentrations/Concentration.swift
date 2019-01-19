ee566db818f2626a5c03e1fd5af39d7c24030be3
Денис Приходкин <den85@ya.ru>
2019-01-20 01:53:23 +0500

    2

--- Concentrations/Concentration.swift
+++ Concentrations/Concentration.swift
@@ -10,10 +10,32 @@ import Foundation
 
 class Concentration
 {
-    var cards = [Card]()
-    var indexOfOneAndOnlyFaceUpCard: Int?
+    private(set) var cards = [Card]()
+    
+    private var indexOfOneAndOnlyFaceUpCard: Int? {
+        get {
+            var foundIndex: Int?
+            for index in cards.indices {
+                if cards[index].isFaceUp {
+                    if foundIndex == nil {
+                        foundIndex = index
+                    } else {
+                        return nil
+                    }
+                }
+            }
+            return foundIndex
+            
+        }
+        set {
+            for index in cards.indices {
+                cards[index].isFaceUp = (index == newValue)
+            }
+        }
+    }
     
     func chooseCard(at index: Int) {
+        assert(cards.indices.contains(index), "Concentration.chooseCard(at: \(index)): chosen index not the cards")
         if !cards[index].isFaceUp {
             if let machIndex = indexOfOneAndOnlyFaceUpCard, machIndex != index {
                 //check if cards mach
@@ -22,20 +44,15 @@ class Concentration
                     cards[index].isMached = true
                 }
                 cards[index].isFaceUp = true
-                indexOfOneAndOnlyFaceUpCard = nil
             } else {
-                //either no cards or 2 cards are face up
-                for flipDownIndex in cards.indices {
-                    cards[flipDownIndex].isFaceUp = false
-                }
-                cards[index].isFaceUp = true
                 indexOfOneAndOnlyFaceUpCard = index
             }
         }
     }
     
     init(numberOfPeirstOfCards: Int) {
-        for _ in 1...numberOfPeirstOfCards {
+        assert(numberOfPeirstOfCards > 0, "Concentration.init(at: \(numberOfPeirstOfCards)): you must have at least one pair of cards")
+        for _ in 0..<numberOfPeirstOfCards {
             let card = Card()
             cards += [card, card]
         }
